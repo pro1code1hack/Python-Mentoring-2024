8ae8db32963f91b48856634d3c3b5d1f529f67e9
juliaozm <ozmiteljul@gmail.com>
2024-07-16 20:05:46 +0100

    julia: homework-3

---
+++ 3. conditional_logic/3. travel_itinerary_planner.py
@@ -0,0 +1,17 @@
+# Plan your future holiday
+# Input: The user enters three cities they plan to visit.
+# Output: The program outputs a travel itinerary in the order entered.
+# Input:
+# >> London
+# >> Paris
+# >> Rome
+# Output:
+# Your travel itinerary: London -> Paris -> Rome
+
+city_1 = input("First city: ")
+city_2 = input("Second city: ")
+city_3 = input("Thrid city: ")
+print ("Your travel itinerary :", city_1, " -> ", city_2, " -> ", city_3)
+
+
+## Is this how it should be?
