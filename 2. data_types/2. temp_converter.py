51c9bea43e18ec4805f3570f5ae97c8c6aa4c356
juliaozm <ozmiteljul@gmail.com>
2024-07-16 20:03:44 +0100

    julia: howework-2

---
+++ 2. data_types/2. temp_converter.py
@@ -0,0 +1,5 @@
+# Write a program that converts a temperature from Fahrenheit to Celsius
+
+temp_fh = float(input("Enter temperature in Fahrenheit: "))
+temp_c = ( temp_fh - 32 ) * 5/9
+print (temp_c, "Celsius")
