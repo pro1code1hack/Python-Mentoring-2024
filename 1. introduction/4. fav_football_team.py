f14edfefe489678455405aa38fcc744a65720b4f
juliaozm <ozmiteljul@gmail.com>
2024-07-16 20:03:00 +0100

    julia: homework-1

---
+++ 1. introduction/4. fav_football_team.py
@@ -0,0 +1,4 @@
+# Write a Python program that asks for the name of a soccer team and then prints a cheer for that team
+
+team = input("Enter your favourite team: ") 
+print(f"{team} is a champion!")
