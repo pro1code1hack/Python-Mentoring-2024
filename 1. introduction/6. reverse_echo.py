6414263c21a000c851df98f5f504224b7d59f29f
juliaozm <ozmiteljul@gmail.com>
2024-07-17 12:36:55 +0100

    deleted old versions

--- 1. introduction/6. reverse_echo.py
+++
@@ -1,7 +0,0 @@
-# Write a Python program that takes in three separate lines of text and then prints them out in a reverse order
-
-line_1 = input("Enter 1st line:")
-line_2 = input("Enter 2nd line:")
-line_3 = input("Enter 3rd line:")
-
-print(line_3, line_2, line_1, sep="\n")
--- 2. data_types/3. interest_calculator.py
+++
@@ -1,8 +0,0 @@
-#  Create a script to calculate simple interest
-
-principal_amount = float(input("Enter the principal amount: "))
-interest_rate = float(input ("Enter the interest rate: "))
-time_in_years = int(input ("Enter the time period in years: "))
-
-calc_interest = principal_amount * interest_rate * time_in_years / 100
-print ("Simple Interest is: ", calc_interest)
