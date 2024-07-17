f067ceaf083a6521cc00c439f24a17935ba1e4bb
Julia <62850656+juliaozm@users.noreply.github.com>
2024-07-17 12:37:51 +0100

    Revert "Homeworks: 1-6"

--- 1.introduction/4.fav_football_team.py
+++
@@ -1,5 +0,0 @@
-"""Write a Python program that asks for the name
-of a soccer team and then prints a cheer for that team"""
-
-fav_team = input("Enter your favourite team: ")
-print(f"{fav_team} is a champion!")
--- 6.data_structures(list)/4.average_of_evens.py
+++
@@ -1,9 +0,0 @@
-"""Generate a list of even elements using range from 
-the input and display the average of even of them"""
-
-list_of_elements = input("Enter a list of numbers (separated by space): ")
-list_of_numbers = [
-    int(char) for char in list_of_elements.split(" ") if int(char) % 2 == 0
-]
-avg = round(sum(list_of_numbers) / len(list_of_numbers))
-print("The average of even numbers is:", avg)
