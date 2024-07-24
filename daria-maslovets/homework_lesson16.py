01e9cb0398ff3f6857141acb8903a47ca6ff7c4b
Daria M <d.v.maslovets@gmail.com>
2024-07-24 23:36:56 +0300

    Complete homework lesson 1-8

---
+++ daria-maslovets/homework_lesson16.py
@@ -0,0 +1,28 @@
+"""
+**Objective:** Create a program that allows the user to manipulate a list in various ways. The user can choose to `add`, `remove`, or `modify` items in the `list`.
+# Example of how the program should work
+# Menu:
+# 1. Add an item
+# 2. Remove an item
+# 3. Modify an item
+# User chooses option 1:
+Enter the item to add: Apple
+Output: List: ['Apple']
+# User chooses option 2:
+Enter the item to remove: Apple
+Output: List: []
+# User chooses option 3:
+Enter the item to modify: Apple
+Enter the new item: Banana
+Output: List: ['Banana']
+
+"""
+
+
+
+
+
+
+
+
+
